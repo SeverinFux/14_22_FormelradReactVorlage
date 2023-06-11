1285d9e770094cfbe4e663c9ae6f31f7fb090a8b
SeverinFux <severin.fux@lernende.bbw.ch>
2023-06-11 14:30:08 +0200

    Commit Message: "Ausgangslage" into Fromelrad pasted for Patch: 0012

--- src/formelrad/Formelrad.js
+++ src/formelrad/Formelrad.js
@@ -9,7 +9,6 @@ export default function Formelrad() {
         i: 2,
         r: ""
     })
-
     return (
         <>
             <section>
@@ -18,9 +17,9 @@ export default function Formelrad() {
                     <img src={formelrad} width="200" alt="Formelrad"/>
                 </header>
                 <form>
-                    <InputField color={"black"} value={values.u} label="Spannung" handleChange={e => {setValues(values => ({...values, u: e.target.value}))}}/>
-                    <InputField color={"black"} value={values.i} label="Stromstärke" handleChange={e => {setValues(values => ({...values, i: e.target.value}))}}/>
-                    <InputField color={"black"} value={values.r} label="Widerstand" handleChange={e => {setValues(values => ({...values, r: e.target.value}))}}/>
+                    <InputField color={"black"} value={values.u} label="Spannung" handleChange={e => {setValues(values => ({...values, u: e.target.value}))}} />
+                    <InputField color={"black"} value={values.i} label="Stromstärke" handleChange={e => {setValues(values => ({...values, i: e.target.value}))}} />
+                    <InputField color={"black"} value={values.r} label="Widerstand" handleChange={e => {setValues(values => ({...values, r: e.target.value}))}} />
                     <button type="submit">Calculate</button>
                 </form>
             </section>
